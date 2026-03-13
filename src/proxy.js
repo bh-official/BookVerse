142287d1b31f7fdef83f698af5ed70cbb8da2583
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-03-13 11:53:44 +0000

    onboarding page works for new user too

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in",
+  "/sign-in/(.*)",
+  "/sign-up",
+  "/sign-up/(.*)",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   // Public routes - allow access
